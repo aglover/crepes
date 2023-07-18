58508a95e1a03dae68384da1d1362455af8a22c5
Andrew Glover <ajglover@gmail.com>
2023-07-18 16:03:09 -0700

    working on gql updates

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ docker compose start
 ```
 
 Note, however, the first time you fire things up, use the `docker compose up` command to bootstrap things. Naturally, to stop things, type `docker compose stop`. This [cheat sheet](https://devhints.io/docker-compose) is nice.
+
+To run the application in development mode (which means recompiling TypeScript on changes and reloading the resultant JS), ensure you run this command:
+
+```
+npm run dev
+```
+
+You'll note that the TS compilation process produces a .js file in the `dist` directory. 
--- index.ts
+++ index.ts
@@ -1,30 +1,85 @@
-import express, { Express, Request, Response } from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
+let sampleWords = [
+  {
+    id: 1,
+    word: "word 1.1",
+    definition: "this is a def 1",
+    example_sentence: "and this is an example 1",
+  },
+  {
+    id: 2,
+    word: "word 2",
+    definition: "this is a def 2",
+    example_sentence: "and this is an example 2",
+  },
+  {
+    id: 3,
+    word: "word 3",
+    definition: "this is a def 3",
+    example_sentence: "and this is an example 3",
+  },
+];
+
 const typeDefs = gql`
- type Query {
-  hello: String
- }
+  type Word {
+    id: ID!
+    word: String!
+    definition: String!
+    example_sentence: String
+  }
+
+  type Query {
+    hello: String
+    words: [Word!]!
+    word(id: ID!): Word!
+  }
+
+  type Mutation {
+    newWord(word: String!): Word!
+  }
 `;
 
 const resolvers = {
   Query: {
     hello: () => {
-      return 'Hello world!'
+      return "Hello world!";
     },
+
+    words: () => {
+      return sampleWords;
+    },
+
+    word: (parent: any, args: { id: number }) => {
+      return sampleWords.find((word) => word.id == args.id);
+    },
+  },
+
+  Mutation: {
+    newWord: (parent: any, args: { word: any; }) => {
+      let wordValue = {
+        id: sampleWords.length + 1,
+        word: args.word,
+        definition: "bal",
+        example_sentence: "blah blah"
+      };
+      sampleWords.push(wordValue);
+      return wordValue;
+    }
   }
 };
 
-const server = new ApolloServer({ typeDefs, resolvers});
+const server = new ApolloServer({ typeDefs, resolvers });
 await server.start();
 
 const app: Express = express();
 const port = process.env.PORT || 8080;
 
-server.applyMiddleware({app, path: '/api'});
+server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port, () => {
   console.log(`⚡️GraphQL server is running at http://localhost:${port}`);
